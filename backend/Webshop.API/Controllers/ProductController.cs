c56ad850fba43ca6a747e41875724548dc877aa2
Attila Sikes <attila.sikes07@gmail.com>
2026-04-15 17:01:52 +0000

    feat: added GET endpoints

---
+++ backend/Webshop.API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+namespace Webshop.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+  private readonly ProductService _productService;
+
+  public ProductController(ProductService productService)
+  {
+    _productService = productService;
+  }
+
+  [HttpGet]
+  public async Task<List<Product>> GetAll() =>
+      await _productService.GetAllAsync();
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<Product>> GetById(string id)
+  {
+    var product = await _productService.GetByIdAsync(id);
+    return product is null ? NotFound() : Ok(product);
+  }
+}
